69ed61ea748810152244db5a17acf53566318ba8
Calinou <calinou@opmbx.org>
2014-04-25 20:59:28 +0200

    More rats.

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -283,7 +283,7 @@ mobs:register_mob("mobs:rat", {
 	light_damage = 0,
 })
 
-mobs:register_spawn("mobs:rat", {"default:dirt_with_grass", "default:stone"}, 8, -1, 2500, 6, 31000)
+mobs:register_spawn("mobs:rat", {"default:dirt_with_grass", "default:stone"}, 8, -1, 1500, 6, 31000)
 
 minetest.register_craftitem("mobs:rat", {
 	description = "Rat",
