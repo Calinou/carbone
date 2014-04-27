f98ac7f770b42ad536f4a578e144983130de5429
Calinou <calinou@opmbx.org>
2014-04-27 12:14:58 +0200

    Made leaf decay faster.

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -306,7 +306,7 @@ minetest.register_abm({
 	neighbors = {"air", "group:liquid"},
 	-- A low interval and a high inverse chance spreads the load
 	interval = 2,
-	chance = 5,
+	chance = 3,
 
 	action = function(p0, node, _, _)
 		--print("leafdecay ABM at "..p0.x..", "..p0.y..", "..p0.z..")")
