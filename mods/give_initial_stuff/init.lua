b8d01dec2cefb706f9b013b87a16982897f23efe
Calinou <calinou@opmbx.org>
2014-04-29 19:43:48 +0200

    Give 1 locked chest to new players.

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -4,6 +4,7 @@ minetest.register_on_newplayer(function(player)
 		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
 		player:get_inventory():add_item('main', 'default:sapling 10')
 		player:get_inventory():add_item('main', 'default:apple 10')
+		player:get_inventory():add_item('main', 'default:chest_locked 1')
 	end
 end)
 
