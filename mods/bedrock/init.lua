c42f209ef7f6bd9977a728e8355ef4de56e38691
Calinou <calinou@opmbx.org>
2014-05-01 18:54:19 +0200

    Better bedrock, generated as an ore for speed. 16 layers of bedrock are now generated to ensure no legitimate player ever sees the void.

--- mods/bedrock/init.lua
+++ mods/bedrock/init.lua
@@ -1,24 +1,31 @@
 -- A simple bedrock mod by jn and Calinou.
 
-local bedrock = {}
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "bedrock:bedrock",
+	wherein        = "default:stone",
+	clust_scarcity = 1*1*1,
+	clust_num_ores = 5,
+	clust_size     = 2,
+	height_min     = -30912,
+	height_max     = -30896,
+})
 
-bedrock.layer = -30912 -- Determined as appropriate by experiment. May change in the future.
-
-minetest.register_on_generated(function(minp, maxp)
-	if maxp.y >= bedrock.layer and minp.y <= bedrock.layer then
-		local pos = {y = bedrock.layer}
-		for x = minp.x,maxp.x do
-		for z = minp.z,maxp.z do
-			pos.x=x; pos.z=z
-			minetest.env:set_node(pos, {name = "bedrock:bedrock"})
-		end
-		end
-	end
-end)
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "bedrock:bedrock",
+	wherein        = "air",
+	clust_scarcity = 1*1*1,
+	clust_num_ores = 5,
+	clust_size     = 2,
+	height_min     = -30912,
+	height_max     = -30896,
+})
 
 minetest.register_node("bedrock:bedrock", {
-	description = "Bedrock",
-	tile_images = {"bedrock.png"},
+	description = "bedrock",
+	tile_images = {"bedrock_bedrock.png"},
 	groups = {unbreakable = 1},
 	sounds = default.node_sound_stone_defaults(),
+	is_ground_content = false,
 })
