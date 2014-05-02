33c48844c08ba4823f1e7752888578adde9e0f83
Calinou <calinou@opmbx.org>
2014-05-02 20:24:11 +0200

    Compress shield textures; add instant bone digging and item dropping if bones can't be placed.

--- mods/bones/init.lua
+++ mods/bones/init.lua
@@ -29,7 +29,7 @@ minetest.register_node("bones:bones", {
 	
 	can_dig = function(pos, player)
 		local inv = minetest.get_meta(pos):get_inventory()
-		return is_owner(pos, player:get_player_name()) and inv:is_empty("main")
+		return is_owner(pos, player:get_player_name()) or inv:is_empty("main")
 	end,
 	
 	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
@@ -59,17 +59,42 @@ minetest.register_node("bones:bones", {
 		end
 	end,
 	
+	on_punch = function(pos, node, player)
+		if(not is_owner(pos, player:get_player_name())) then
+			return
+		end
+		
+		local inv = minetest.get_meta(pos):get_inventory()
+		local player_inv = player:get_inventory()
+		local has_space = true
+		
+		for i=1,inv:get_size("main") do
+			local stk = inv:get_stack("main", i)
+			if player_inv:room_for_item("main", stk) then
+				inv:set_stack("main", i, nil)
+				player_inv:add_item("main", stk)
+			else
+				has_space = false
+				break
+			end
+		end
+		
+		-- remove bones if player emptied them
+		if has_space then
+			minetest.remove_node(pos)
+		end
+	end,
+	
 	on_timer = function(pos, elapsed)
 		local meta = minetest.get_meta(pos)
-		local time = meta:get_int("time")+elapsed
 		local publish = 600
-		if tonumber(minetest.setting_get("share_bones_time")) then
-			publish = tonumber(minetest.setting_get("share_bones_time"))
+		if tonumber(minetest.setting_get("bones_share_time")) then
+			publish = tonumber(minetest.setting_get("bones_share_time"))
 		end
 		if publish == 0 then
 			return
 		end
-		if time >= publish then
+		if minetest.get_gametime() >= meta:get_int("time") + publish then
 			meta:set_string("infotext", meta:get_string("owner").."'s old bones")
 			meta:set_string("owner", "")
 		else
@@ -88,18 +113,28 @@ minetest.register_on_dieplayer(function(player)
 	pos.y = math.floor(pos.y+0.5)
 	pos.z = math.floor(pos.z+0.5)
 	local param2 = minetest.dir_to_facedir(player:get_look_dir())
+	local player_name = player:get_player_name()
+	local player_inv = player:get_inventory()
+
+	if minetest.check_player_privs(player_name, {interact=false}) and
+	not minetest.is_singleplayer() then
+	   return
+	end
 	
 	local nn = minetest.get_node(pos).name
 	if minetest.registered_nodes[nn].can_dig and
 		not minetest.registered_nodes[nn].can_dig(pos, player) then
-		local player_inv = player:get_inventory()
 
+		-- drop items instead of delete
 		for i=1,player_inv:get_size("main") do
-			player_inv:set_stack("main", i, nil)
+			minetest.add_item(pos, player_inv:get_stack("main", i))
 		end
 		for i=1,player_inv:get_size("craft") do
-			player_inv:set_stack("craft", i, nil)
+			minetest.add_item(pos, player_inv:get_stack("craft", i))
 		end
+		-- empty lists main and craft
+		player_inv:set_list("main", {})
+		player_inv:set_list("craft", {})
 		return
 	end
 	
@@ -109,28 +144,31 @@ minetest.register_on_dieplayer(function(player)
 	
 	local meta = minetest.get_meta(pos)
 	local inv = meta:get_inventory()
-	local player_inv = player:get_inventory()
 	inv:set_size("main", 8*4)
-	
-	local empty_list = inv:get_list("main")
 	inv:set_list("main", player_inv:get_list("main"))
-	player_inv:set_list("main", empty_list)
+	player_inv:set_list("main", {})
+	player_inv:set_list("craft", {})
 	
 	for i=1,player_inv:get_size("craft") do
-		inv:add_item("main", player_inv:get_stack("craft", i))
-		player_inv:set_stack("craft", i, nil)
+		local stack = player_inv:get_stack("craft", i)
+		if inv:room_for_item("main", stack) then
+			inv:add_item("main", stack)
+		else
+			--drop if no space left
+			minetest.add_item(pos, stack)
+		end
 	end
 	
 gui_bg_img = "background[5,5;1,1;gui_formbg.png;true]"
 gui_slots = "listcolors[#606060AA;#606060;#141318;#30434C;#FFF]"
 
-	meta:set_string("formspec", "size[8,9;]"..gui_bg_img..gui_slots..
+	meta:set_string("formspec", "size[8,9;]".. gui_bg_img .. gui_slots ..
 			"list[current_name;main;0,0;8,4;]"..
 			"list[current_player;main;0,5;8,4;]")
-	meta:set_string("infotext", player:get_player_name().."'s fresh bones")
-	meta:set_string("owner", player:get_player_name())
-	meta:set_int("time", 0)
+	meta:set_string("infotext", player_name .. "'s fresh bones")
+	meta:set_string("owner", player_name)
+	meta:set_int("time", minetest.get_gametime())
 	
-	local timer  = minetest.get_node_timer(pos)
+	local timer = minetest.get_node_timer(pos)
 	timer:start(10)
 end)
