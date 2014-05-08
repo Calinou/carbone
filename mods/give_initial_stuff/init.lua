8a2b5d380b857353d5401370154576d50ef6aecc
Calinou <calinou@opmbx.org>
2014-05-08 22:48:45 +0200

    Give new players a wooden pickaxe and 10 torches.

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -2,6 +2,8 @@ minetest.register_on_newplayer(function(player)
 	--print("on_newplayer")
 	if minetest.setting_getbool("give_initial_stuff") then
 		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
+		player:get_inventory():add_item("main", "default:pick_wood")
+		player:get_inventory():add_item("main", "default:torch 10")
 		player:get_inventory():add_item("main", "default:sapling 10")
 		player:get_inventory():add_item("main", "default:apple 10")
 		player:get_inventory():add_item("main", "default:chest_locked 1")
