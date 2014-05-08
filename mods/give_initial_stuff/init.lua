69ceec7cbe5f39257c5b5f85377da75010d4d93e
Calinou <calinou@opmbx.org>
2014-05-08 22:55:16 +0200

    Announce new player joins regardless of give_initial_stuff.

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -1,7 +1,7 @@
 minetest.register_on_newplayer(function(player)
-	--print("on_newplayer")
+	minetest.log("action", player:get_player_name() .. " joined for the first time.")
 	if minetest.setting_getbool("give_initial_stuff") then
-		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
+		minetest.log("action", "Giving initial stuff to player " .. player:get_player_name() .. ".")
 		player:get_inventory():add_item("main", "default:pick_wood")
 		player:get_inventory():add_item("main", "default:torch 10")
 		player:get_inventory():add_item("main", "default:sapling 10")
