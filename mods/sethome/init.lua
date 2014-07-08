338f2ffeeaadcc4d678d96d88dd546cf864d35a2
Calinou <calinou@opmbx.org>
2014-07-08 11:15:31 +0200

    Register the /spawn command even if no static spawn point is present, so that the spawn can be changed without having to restart the server.

--- mods/sethome/init.lua
+++ mods/sethome/init.lua
@@ -36,9 +36,9 @@ minetest.register_chatcommand("home", {
         end
         if homepos[player:get_player_name()] then
             player:setpos(homepos[player:get_player_name()])
-            minetest.chat_send_player(name, "Teleporting to home.")
+            minetest.chat_send_player(name, "[T] Teleporting to home.")
         else
-            minetest.chat_send_player(name, "Use /sethome to set a home first.")
+            minetest.chat_send_player(name, "[T] Use /sethome to set a home first.")
         end
     end,
 })
@@ -50,7 +50,7 @@ minetest.register_chatcommand("sethome", {
         local player = minetest.get_player_by_name(name)
         local pos = player:getpos()
         homepos[player:get_player_name()] = pos
-        minetest.chat_send_player(name, "Home set.")
+        minetest.chat_send_player(name, "[T] Home set.")
         changed = true
         if changed then
         	local output = io.open(homes_file, "w")
@@ -63,17 +63,17 @@ minetest.register_chatcommand("sethome", {
     end,
 })
 
-if minetest.setting_get("static_spawnpoint") then
-    minetest.register_chatcommand("spawn", {
-        description = "Teleports you to the spawn point",
-        privs = {spawn = true},
-        func = function(name)
-            local player = minetest.get_player_by_name(name)
-            if player == nil then return false -- Check to prevent the server crashing.
-            else
-                player:setpos(minetest.setting_get_pos("static_spawnpoint"))
-                minetest.chat_send_player(name, "Teleporting to spawn.")
-            end
-        end,
-    })
-end
+minetest.register_chatcommand("spawn", {
+    description = "Teleports you to the spawn point",
+    privs = {spawn = true},
+    func = function(name)
+        local player = minetest.get_player_by_name(name)
+        if player == nil then return false end -- Check to prevent the server crashing.
+        if minetest.setting_get("static_spawnpoint") then
+            player:setpos(minetest.setting_get_pos("static_spawnpoint"))
+            minetest.chat_send_player(name, "[T] Teleporting to spawn.")
+        else
+            minetest.chat_send_player(name, "[T] No static spawn point is set.")
+        end
+    end,
+})
